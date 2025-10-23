f55fdfc370ffa2b07f79e7e9519b8bc1252c5d38
dimitrisdiama <dimitrisdiama2@gmail.com>
2025-10-23 22:38:34 +0300

    add:models and factory

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        Home
+    </x-slot:heading>
+    <p>Home Page</p>
+</x-layout>
---
+++ resources/views/jobs.blade.php
@@ -0,0 +1,17 @@
+<x-layout>
+    <x-slot:heading>
+        Jobs Listings
+    </x-slot:heading>
+
+    <ul>
+        @foreach ($jobs as $job)
+            <a href="/jobs/{{$job['id']}}" class="text-blue-500 hover:underline">
+                <li>
+                    <strong>{{ $job['title'] }}:</strong>
+                    Salary {{ $job['salary'] }} per year.
+                </li>
+            </a>
+        @endforeach
+    </ul>
+
+</x-layout>

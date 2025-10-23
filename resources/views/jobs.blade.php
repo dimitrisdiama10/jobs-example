a73a5de5307d1dbc5e384025bbbb01206aabdf42
dimitrisdiama <dimitrisdiama2@gmail.com>
2025-10-23 23:42:14 +0300

    feat: add employer name in job

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,15 +3,17 @@
         Jobs Listings
     </x-slot:heading>
 
-    <ul>
+    <div class="space-y-4">
         @foreach ($jobs as $job)
-            <a href="/jobs/{{$job['id']}}" class="text-blue-500 hover:underline">
-                <li>
-                    <strong>{{ $job['title'] }}:</strong>
-                    Salary {{ $job['salary'] }} per year.
-                </li>
+            <a href="/jobs/{{$job['id']}}" class="block border border-gray-400 px-4 py-6 rounded-lg">
+                <div class="font-bold text-blue-400">
+                    {{ $job->employer->name }}
+                </div>
+                <div>
+                    <strong>{{ $job['title'] }}:</strong> Salary {{ $job['salary'] }} per year.
+                </div>
             </a>
         @endforeach
-    </ul>
+    </div>
 
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,18 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('jobs', function ()  {
+Route::get('jobs', function () {
+    $job = Job::with('employer')->get();
+
     return view(
         'jobs',
         [
-            'jobs' => Job::all()
+            'jobs' => $job
         ]
     );
 });
 
-Route::get('/jobs/{id}', function ($id)  {
+Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
     return view('job', ['job' => $job]);
